76756c4fa35e0dd40d4c86b07647cbe6b7f94507
Visal Suos <visalsuos@Visals-MacBook-Pro.local>
2020-12-27 12:18:39 +0700

    update

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server)
+const app = require('express')()
+const http = require('http').createServer(app)
 
 
-io.socket.on("connection", (userSocket) => {
+app.get('/', (req, res) => {
+    res.send("Node Server is running. Yay!!")
+})
+
+//Socket Logic
+const socketio = require('socket.io')(http)
+
+socketio.on("connection", (userSocket) => {
     userSocket.on("send_message", (data) => {
         userSocket.broadcast.emit("receive_message", data)
     })
 })
 
-server.listen(process.env.PORT || 3000);
+http.listen(process.env.PORT || 3000)
