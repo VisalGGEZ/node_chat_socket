1d1790fa34a33171ddd8cb0c1324f2801f3fdba7
Visal Suos <visalsuos@Visals-MacBook-Pro.local>
2020-12-27 10:46:41 +0700

    update code

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const app = require('express')()
-const http = require('http').createServer(app)
-
+const wss = require('ws').createServer(app)
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send("Node Server is running. Yay!!")
 })
 
 //Socket Logic
-const socketio = require('socket.io')(http)
+const socketio = require('socket.io')(wss)
 
 socketio.on("connection", (userSocket) => {
+    console.log('Client connected');
     userSocket.on("send_message", (data) => {
         userSocket.broadcast.emit("receive_message", data)
     })
 })
 
-http.listen(process.env.PORT)
+http.listen(PORT, ()=>console.log(`Listening on ${PORT}`))
