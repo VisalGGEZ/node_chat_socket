4155cb18bd39f2f757eedf7d1811c15093e5f087
Visal Suos <visalsuos@Visals-MacBook-Pro.local>
2020-12-27 10:50:29 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('express')()
-const wss = require('ws').createServer(app)
+const http = require('http').createServer(app)
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
@@ -7,10 +7,9 @@ app.get('/', (req, res) => {
 })
 
 //Socket Logic
-const socketio = require('socket.io')(wss)
+const socketio = require('socket.io')(http)
 
 socketio.on("connection", (userSocket) => {
-    console.log('Client connected');
     userSocket.on("send_message", (data) => {
         userSocket.broadcast.emit("receive_message", data)
     })
